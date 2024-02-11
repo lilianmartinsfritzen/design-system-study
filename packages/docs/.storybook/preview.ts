7ce9aa90e223c68bbd58c93fe9bcec1799edfb90
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2024-02-11 14:55:25 -0300

    feature(design-system): adjustments to create documentation in markdown

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming';
 
 const preview: Preview = {
   parameters: {
@@ -12,6 +13,9 @@ const preview: Preview = {
     backgrounds: {
       default: 'dark'
     },
+    docs: {
+      theme: themes.dark
+    }
   },
 };
 
