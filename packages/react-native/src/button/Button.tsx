2bb07033da28575edf50920244e999b7f8c83ffa
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2024-03-21 17:47:44 -0300

    feature: in this commit it was possible to build the component in the correct folder and with the appropriate typing - Button component

---
+++ packages/react-native/src/button/Button.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+export interface ButtonProps {
+  variant?: "primary" | "secondary" | "tertiary";
+  size?: "sm" | "md";
+  children: React.ReactNode;
+  isDisabled?: boolean;
+  onPress?: () => void;
+}
+
+export const Button = ({
+  variant = "primary",
+  size = "md",
+  children,
+  isDisabled,
+  onPress,
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.button,
+        styles[variant],
+        styles[size],
+        isDisabled && styles.disabled,
+      ]}
+      disabled={isDisabled}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 14,
+    fontSize: 14,
+    fontWeight: "500",
+    minWidth: 120,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  text: {
+    fontFamily: "Nunito-Regular",
+  },
+  primary: {
+    backgroundColor: "#0096c7",
+    color: "#fff",
+  },
+  secondary: {
+    borderColor: "#0096c7",
+    borderWidth: 2,
+    color: "#48cae4",
+  },
+  tertiary: {
+    borderColor: "#7c7c8a",
+    borderWidth: 2,
+    color: "#7c7c8a",
+  },
+  disabled: {
+    backgroundColor: "#a9a9b2",
+  },
+  sm: {
+    height: 38,
+  },
+  md: {
+    height: 46,
+  },
+});
